4c2363e2e847c6877dc33821bdb27c631b4ebcd0
Alvaro <dralvphd2@gmail.com>
2022-08-02 20:46:23 -0300

    Realizando desafios

---
+++ desafios/desafio1.js
@@ -0,0 +1,7 @@
+"use strict";
+let employee = {
+    code: 12,
+    name: "Alvaro"
+};
+employee.code = 10;
+employee.name = "John";
---
+++ desafios/desafio1.ts
@@ -0,0 +1,7 @@
+let employee:{code:number,name:string} ={
+  code: 12,
+  name: "Alvaro"
+}
+
+employee.code = 10;
+employee.name = "John";
